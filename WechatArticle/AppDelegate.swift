497f60c98a338485fa21bc9df290c6db3eccacb2
huang1988519 <huang1988519@126.com>
2015-11-17 20:59:10 +0800

    modify

--- WechatArticle/AppDelegate.swift
+++ WechatArticle/AppDelegate.swift
@@ -25,12 +25,12 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         
         return true
     }
-    func application(application: UIApplication, handleOpenURL url: NSURL) -> Bool {
-        return ShareSDK.handleOpenURL(url, wxDelegate: self)
-    }
-    func application(application: UIApplication, openURL url: NSURL, sourceApplication: String?, annotation: AnyObject) -> Bool {
-        return ShareSDK.handleOpenURL(url, sourceApplication: sourceApplication, annotation: annotation, wxDelegate: self)
-    }
+//    func application(application: UIApplication, handleOpenURL url: NSURL) -> Bool {
+//        return ShareSDK.handleOpenURL(url, wxDelegate: self)
+//    }
+//    func application(application: UIApplication, openURL url: NSURL, sourceApplication: String?, annotation: AnyObject) -> Bool {
+//        return ShareSDK.handleOpenURL(url, sourceApplication: sourceApplication, annotation: annotation, wxDelegate: self)
+//    }
     
     //MARK: -- 
     func config() {
@@ -43,7 +43,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         //配置 leanCloud
         AVOSCloud.setApplicationId("qfrsSEumQvfkvyR7gMSXErKg", clientKey: "nTSTQrCGKDFm9zQoexAJHhGW")
         //配置SharedSDK
-        ShareSDK.registerApp("c640d0a572fb")
+//        ShareSDK.registerApp("c640d0a572fb")
         //配置缓存
         cacheManager.calculateDiskCacheSizeWithCompletionHandler { (size) -> () in
             log.debug("图片缓存已占用:\(size)")
@@ -57,7 +57,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
      - returns: nil
      */
     func initializePlat() {
-        ShareSDK.connectSinaWeiboWithAppKey("", appSecret: "", redirectUri: "")
+//        ShareSDK.connectSinaWeiboWithAppKey("", appSecret: "", redirectUri: "")
     }
 }
 
--- WechatArticle/WechatArticle-Bridging-Header.h
+++ WechatArticle/WechatArticle-Bridging-Header.h
@@ -2,4 +2,4 @@
 //  Use this file to import your target's public headers that you would like to expose to Swift.
 //
 
-#import <ShareSDK/ShareSDK.h>
+//#import <ShareSDK/ShareSDK.h>
